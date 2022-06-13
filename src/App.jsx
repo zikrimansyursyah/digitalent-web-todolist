c60db7f66b06d2c109a22c0158b07cda45f14bf6
Zikri Mansyursyah <86043071+zikrimansyursyah@users.noreply.github.com>
2022-06-13 13:22:27 +0700

    little update

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,49 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import Card from "./components/card";
+import CardDetail from "./components/CardDetail";
 
 function App() {
   const [inputAdd, setInputAdd] = useState(null);
-  const [listCard, setListCard] = useState([
-  ]);
+  const [listCard, setListCard] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [modal, setModal] = useState(true);
 
   useEffect(() => {
     const taskList = window.localStorage.getItem("task-list");
-    if (taskList !== null || (taskList || []).length !== 0) {
-      setListCard(JSON.parse(taskList));
-    }
+    const totalTask = window.localStorage.getItem("total-task");
+
+    setListCard(
+      taskList !== null || (taskList || []).length !== 0
+        ? JSON.parse(taskList)
+        : []
+    );
+    setTotal(totalTask !== null ? parseInt(totalTask) : 0);
   }, []);
 
   useEffect(() => {
+    console.info(listCard);
     if (listCard.length > 0) {
       window.localStorage.setItem("task-list", JSON.stringify(listCard));
     }
-    document.querySelector('.board').scrollTo(0, document.querySelector(".board").scrollHeight)
-  }, [listCard]);
+    window.localStorage.setItem("total-task", total);
+    document
+      .querySelector(".board")
+      .scrollTo(0, document.querySelector(".board").scrollHeight);
+  }, [listCard, total]);
 
   function handleAdd(event) {
     event.preventDefault();
     if (!inputAdd) return;
-    setListCard((prev) => ([...prev, {
-      title: inputAdd,
-      theme: `TS-${listCard.length + 1}`,
-      badges: null,
-    }]));
+    setListCard((prev) => [
+      ...prev,
+      {
+        title: inputAdd,
+        theme: `TS-${total + 1}`,
+        badges: null,
+      },
+    ]);
+    setTotal((prev) => prev + 1);
     document.getElementById("add-task").value = "";
     setInputAdd(null);
   }
@@ -41,17 +56,60 @@ function App() {
       <div className="board mx-auto">
         <div className="board-header">Task</div>
         <div className="task-wrapper">
-          {
-            (listCard || []).map((item, index) => (
-              <Card key={index} title={item.title} theme={item.theme} badges={item.badges} setListCard={setListCard} />
-            ))
-          }
+          {(listCard || []).map((item, index) => (
+            <Card
+              key={index}
+              title={item.title}
+              theme={item.theme}
+              badges={item.badges}
+              setListCard={setListCard}
+            />
+          ))}
         </div>
         <form className="board-footer" onSubmit={handleAdd}>
-          {/* <label htmlFor="">Enter</label> */}
-          <input type="text" placeholder="add new task" id="add-task" onChange={(event) => { setInputAdd(event.target.value) }} />
-          <button type="submit" className="btn btn-primary">Add</button>
+          <input
+            type="text"
+            placeholder="add new task"
+            id="add-task"
+            onChange={(event) => {
+              setInputAdd(event.target.value);
+            }}
+          />
+          <button type="submit" className="btn btn-primary">
+            Add
+          </button>
         </form>
+      </div>
+      {/* <CardDetail isOpen={modal} /> */}
+      <div class="modal fade show" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Modal title</h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              <p>Modal body text goes here.</p>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/components/CardDetail.jsx
+++ src/components/CardDetail.jsx
@@ -1,3 +1,35 @@
 export default function CardDetail({ isOpen }) {
-
+  if (isOpen)
+    return (
+      <div class="modal fade show" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Modal title</h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              <p>Modal body text goes here.</p>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
 }
